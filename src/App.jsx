f3d29c780fbea51acddee43d27d639cb22351da7
Elayabarathi M V <elayabarathiedison@gmail.com>
2025-04-22 20:50:27 +0530

    Footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import Todo from './components/Todo'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='bg-stone-900 grid py-4 min-h-screen'>
-      <Todo />
-    </div>
-  )
+    <>
+      <div className="bg-stone-900 grid py-4 min-h-max h-screen items-end">
+        <Todo />
+        <Footer />
+      </div>
+      
+    </>
+  );
 }
 
 export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <div className="text-white flex justify-center">
+        <p className="text-zinc-500 text-sm reveal-up">
+          &copy; 2025 <span className="text-zinc-200">· Panda Productions</span>
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Footer
